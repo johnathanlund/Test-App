7058bc5818dd0fd22a429ff8d6e84eb9ca8e65b1
johnathanlund <jalshield@gmail.com>
2017-03-04 23:25:28 -0700

    Updates to the file upload, with ability to cancel individual uploads, including on multiple, as well as all at once. Clearing the queue as well on upload now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,12 +23,35 @@ export class AppComponent implements OnInit {
  //override the onAfterAddingfile property of the uploader so it doesn't authenticate with //credentials.
    this.uploader.onAfterAddingFile = (file)=> { file.withCredentials = false;
      console.log('In .onAfterAddingFile, jsonof file is:  ' + file.file.name);
-     console.log('Json of file.file is: ' + JSON.stringify(file.file))
+     console.log('Json of file.file is: ' + JSON.stringify(file.file));
+
    };
+
  //overide the onCompleteItem property of the uploader so we are
  //able to deal with the server response.
    this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
          console.log("ImageUpload:uploaded:", item, " Thats the item. ", status, " Thats the status. ", response, " THats the response.", item.file.name );
+         this.uploader.queue.length = 0;
+
+         console.log('What is in the uploader.queue is: ' + this.uploader.queue.length);
      };
+ }
+ // fileEvent(fileInput: any) {
+ //   let file = fileInput.target.files[0];
+ //   let fileName = file.name;
+ //   console.log('FileName function has been reached. FileName is: ' + fileName);
+ // };
+ cancelUploaderItem(item) {
+   for(var i = 0; i<this.uploader.queue.length; i++) {
+    var itm = this.uploader.queue[i];
+    if(itm=item) {
+      itm.remove();
+    }
+   }
+   console.log('File has been cleared from the uploader.queue');
+ };
+ cancelUploaderAll() {
+   this.uploader.clearQueue();
+   console.log("Uploader.queue has been cleared.");
  }
 }
